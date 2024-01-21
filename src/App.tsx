e9ccbf3f52432d6d06aa9379d5cbb233cc006141
danny <sml.iz@mail.ru>
2024-01-21 17:51:23 +0400

    0.4.1 - 21.01.2023 - 17:51

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Helmet } from 'react-helmet-async';
 import { WindowContainer } from 'global';
 import { HomePage, AboutPage } from './pages';
 
+import imgOg from '/og-photo.png';
+
 function App() {
     return (
         <>
@@ -13,10 +15,10 @@ function App() {
 
                 <meta property='og:type' content='website' />
                 <meta property='og:site_name' content='KITTEN20.EXE' />
-                <meta property='og:image' content='og-photo.png' />
+                <meta property='og:image' content={imgOg} />
                 <meta property='og:description' content='An 18-Year-Old Frontend Developer from Russia, who loves old-school games, delicious pizza, Breakcore music and JavaScript!' />
                 
-                <meta property='twitter:image' content='og-photo.png' />
+                <meta property='twitter:image' content={imgOg} />
                 <meta property='twitter:card' content='summary_large_image' />
                 <meta property='twitter:description' content='An 18-Year-Old Frontend Developer from Russia, who loves old-school games, delicious pizza, Breakcore music and JavaScript!' />
             </Helmet>
