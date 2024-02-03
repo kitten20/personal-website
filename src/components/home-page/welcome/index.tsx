88609615f59f31d92b00d014765a3fb4cd8fa6b8
danny <sml.iz@mail.ru>
2024-02-03 12:09:17 +0400

    0.6.1 - 03.02.2023 - 12:09

--- src/components/home-page/welcome/index.tsx
+++ src/components/home-page/welcome/index.tsx
@@ -14,6 +14,7 @@ function Welcome() {
                     <S.TitleWelcome>Hi, I'm Daniil Ulyanov!</S.TitleWelcome>
                     <S.TitleTopLinks>
                         <p>a.k.a.</p>
+                        <a href="https://t.me/qMilly">qMilly,</a>
                         <a href="https://discordapp.com/users/426331342436040704">K1tten_21,</a>
                         <a href="https://soundcloud.com/qrellyq/tracks">Relly Q - Almost Dead,</a>
                         <a href="https://github.com/kitten20">Kitten20.</a>
