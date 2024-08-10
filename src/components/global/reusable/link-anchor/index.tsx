af9f0960c0e9485aef405acce14ae22bffbccf3f
danny <sml.iz@mail.ru>
2024-08-11 03:03:47 +0400

    fixin anchor-link - 02:03

--- src/components/global/reusable/link-anchor/index.tsx
+++ src/components/global/reusable/link-anchor/index.tsx
@@ -1,8 +1,17 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 import { LinkProps } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 
 function LinkAnchor({children, ...linkProps}: PropsWithChildren<LinkProps>) {
+    useEffect(() => {
+        const { hash } = window.location;
+        if (hash) {
+            const id = hash.replace('#', '');
+            const element = document.getElementById(id);
+            if (element) element.scrollIntoView();
+        }
+    }, []);
+
     return ( <HashLink {...linkProps} smooth>{children}</HashLink> );
 }
 
